46ae43447429517440625bd26fa1092661d1dae8
Chad Wallace <cewall0@gmail.com>
2024-07-22 14:38:55 -0400

    Initial Commit

---
+++ OrganicPractice/OrganicPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganicPracticeApp.swift
+//  OrganicPractice
+//
+//  Created by Chad Wallace on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct OrganicPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
